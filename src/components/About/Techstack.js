5f664559a36a867bfab7bb82a4e77ab57aac0756
Rakshit Pandey <rakshit.lucky@gmail.com>
2024-07-03 19:39:05 +0530

    changes

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -1,16 +1,18 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
 import {
+  DiHtml5,
+  DiCss3,
   DiJavascript1,
   DiReact,
   DiNodejs,
   DiMongodb,
-  DiJava,
+  DiJava
 } from "react-icons/di";
 import {
-  SiSolidity,
+  SiExpress,
   SiBootstrap,
-  SiMysql,
+  SiMysql
 } from "react-icons/si";
 
 function Techstack() {
@@ -20,7 +22,16 @@ function Techstack() {
         <DiJava />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
+        <DiHtml5 />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiCss3 />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
         <DiJavascript1 />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiBootstrap />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiReact />
@@ -29,13 +40,10 @@ function Techstack() {
         <DiNodejs />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
+        <SiExpress />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
         <DiMongodb />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiSolidity />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiBootstrap />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiMysql />
